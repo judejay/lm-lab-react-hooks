42d0eec55674883fbc3661e34df69e70fb0ea750
judejay <judejay26@gmail.com>
2023-12-17 23:06:49 +0000

    Feat: use effect

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,36 @@
-// make an api call using useEffect
+import { useEffect, useState } from "react";
+
+interface ToDo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 export const APICall = () => {
+  const [json, setJson] = useState<ToDo | null>(null);
+  useEffect(() => {
+    const fetchToDo = async () => {
+      try {
+        setJson(
+          await (
+            await fetch("https://jsonplaceholder.typicode.com/todos/1")
+          ).json()
+        );
+      } catch (error) {
+        /* empty */
+      }
+    };
+    fetchToDo();
+  }, []);
 
-    // Your code here!
-
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <div>
+      <h2>useEffect</h2>
+      <p>{json?.id}</p>
+      <p>{json?.title}</p>
+      <p>{json?.completed}</p>
+      <p>{json?.userId}</p>
+    </div>
+  );
+};
